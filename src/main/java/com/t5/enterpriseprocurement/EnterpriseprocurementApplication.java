3389c9cdc5235da2052984b83a43d983e1ae62dd
Goutham Raj <goutham@Gouthams-MacBook-Air.local>
2026-07-09 01:43:14 +0530

    Milestone 1: Spring Boot setup and MySQL configuration

---
+++ src/main/java/com/t5/enterpriseprocurement/EnterpriseprocurementApplication.java
@@ -0,0 +1,13 @@
+package com.t5.enterpriseprocurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseprocurementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseprocurementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t5/enterpriseprocurement/EnterpriseprocurementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t5.enterpriseprocurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseprocurementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
